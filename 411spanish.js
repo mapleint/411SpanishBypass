02671a1c183e0150af34e818e1864af9dea865f1
mapleint <albertarlington21@gmail.com>
2021-04-23 09:50:02 -0700

    Add files via upload

---
+++ 411spanish.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name     411spanishPOC
+// @version  0.5
+// @include https://spanish411.net/Practiquemos.asp
+// @include http://spanish411.net/Practiquemos.asp
+// @grant    pee
+// @run-at document-idle
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    i = total;
+    attempt = total; // score = total/attempt
+    secs = Math.round((startTime * .6) - ((startTime * Math.random()) *.4));
+    endPractice(";)");
+})();
